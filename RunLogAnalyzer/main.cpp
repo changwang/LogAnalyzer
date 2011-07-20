2c12cf547f99af8f1d58dcf4305755ff1362da83
Chang Wang <changwang83@gmail.com>
2011-07-20 11:57:02 -0400

    used high resolution timing

--- RunLogAnalyzer/main.cpp
+++ RunLogAnalyzer/main.cpp
@@ -1,16 +1,28 @@
 #include <map>
 #include <vector>
 #include <ctime>
+#include <windows.h>
 #include "Parser.h"
 #include "Log.h"
 #include "LogEntry.h"
 
 int main()
 {
-    time_t startTime = time(NULL);
+    LARGE_INTEGER li;
+    __int64 CounterStart = 0;
+    __int64 CounterEnd = 0;
+    double PCFreq = 0.0;
+
     Parser parser;
     Log log(parser.GetZ3Context(), "1.txt");
     log.ParseLog();
+
+    if (!QueryPerformanceFrequency(&li))
+        cout << "QueryPerformanceFrequency failed!\n";
+
+    PCFreq = double(li.QuadPart)/1000.0;
+    QueryPerformanceCounter(&li);
+    CounterStart = li.QuadPart;
 
     map<string, vector<LogEntry> > mp = log.GetParsedAddresses();
     map<string, vector<LogEntry> >::iterator mitr;
@@ -18,8 +30,10 @@ int main()
     {
         parser.Start(&log, mitr->first, parser.DumpValue(mitr->first));
     }
-    time_t endTime = time(NULL);
-    cout << "Time elapsed: " << difftime(endTime, startTime) << endl;
-    cout << Z3_model_to_string(parser.GetZ3Context(), parser.GetResult());
+
+    QueryPerformanceCounter(&li);
+    CounterEnd = li.QuadPart;
+    cout << "Time elapsed: " << double(CounterEnd-CounterStart)/PCFreq << endl;
+    //cout << Z3_model_to_string(parser.GetZ3Context(), parser.GetResult());
     return 0;
 }
